7fe23a6d3028ab74cbf1cdef5ed1062c95b754e5
ForkKILLET <fork_killet@qq.com>
2019-09-17 21:53:54 +0800

    test: 接轨 Msh (1)

---
+++ main.js
@@ -0,0 +1,6 @@
+// Note: 在此 js 之前，应先加载 https://icelava.ga/main.js
+
+$(document).ready(function()
+{
+    $("#user_op").prepend(`<a href="http://icelava.ga/OI-Nazo/level_tree.html" class="text"><i class="fa fa-tree"></i> Level Tree</a>`);
+});
